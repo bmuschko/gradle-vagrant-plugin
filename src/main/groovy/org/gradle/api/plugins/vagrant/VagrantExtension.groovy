c1e94980b1c00ac88da14eeb64bafd926a90b408
bjornmagnusson <bjornmagnusson1984@gmail.com>
2014-05-26 23:43:25 +0200

    Added Installation properties to Vagrant extensions

--- src/main/groovy/org/gradle/api/plugins/vagrant/VagrantExtension.groovy
+++ src/main/groovy/org/gradle/api/plugins/vagrant/VagrantExtension.groovy
@@ -33,7 +33,16 @@ class VagrantExtension {
      */
     EnvironmentVariables environmentVariables = new EnvironmentVariables()
 
+	/**
+	 * Installation variables
+	 */
+	Installation installation = new Installation()
+	
     void environmentVariables(Closure closure) {
         ConfigureUtil.configure(closure, environmentVariables)
     }
+	
+	void installation(Closure closure) {
+		ConfigureUtil.configure(closure, installation)
+	}
 }
