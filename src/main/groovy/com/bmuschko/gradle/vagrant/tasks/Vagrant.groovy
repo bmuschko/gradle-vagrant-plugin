120c0da38b39d9f8a869630c1c728f503fd6ee64
Benedikt Ritter <beneritter@gmail.com>
2021-02-15 20:16:40 +0100

    Remove unused imports

--- src/main/groovy/com/bmuschko/gradle/vagrant/tasks/Vagrant.groovy
+++ src/main/groovy/com/bmuschko/gradle/vagrant/tasks/Vagrant.groovy
@@ -28,7 +28,6 @@ import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
